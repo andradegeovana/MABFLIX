942d88e6e77508a89c1230959821a6868538f7a4
andradegeovana <geovanaandrade@edu.unifil.br>
2023-05-12 16:56:14 -0300

    Atualizacao politica

--- politica.py
+++ politica.py
@@ -2,19 +2,27 @@ import random
 class Politica():
    bracos = ["acao", "aventura", "romance", "terror", "infantil"]
    
-   def __init__(self,recompensas) -> None:
+   # Inicializa as recompensas
+   def __init__(self, recompensas):
       self.recompensas = recompensas
       
+   # Seleciona o braco randomicamente
    def processo(self):
-      self.var = random.random()
-      if self.var < 0.5:
-         bracosAleatorios = random.randint(1,5)
-         print(self.bracos[bracosAleatorios])
-         print(bracosAleatorios)
-      
-      self.recompensas[bracosAleatorios] += 1
-      print(self.recompensas)
-      print(self.var)
+      for i in range(10):
+         # var (numero randomico) recebe um valor aleatorio entre 0 e 1
+         self.var = random.random()
+         # Se o var for menor que o epsilon, escolhe um braco aleatorio
+         if self.var < 0.5:
+            # Escolhe um braco aleatorio entre todos os bracos
+            bracosAleatorios = random.randint(0,4)
+            print(self.bracos[bracosAleatorios])
+            print(bracosAleatorios)
+         
+         # Da uma recompensa ao braco escolhido
+         self.recompensas[bracosAleatorios] += 1
+         print(self.recompensas)
+         print(self.var)
+
 recompensas = [1,1,1,1,1]
 politica = Politica(recompensas)
 #number = random(0,1)
