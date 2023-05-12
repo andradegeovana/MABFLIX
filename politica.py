0cc8d9a6ca5c216a713ce65080cd520e6a581a54
Sergio Kenji Sawasaki Tanaka <105051554+SerginhoTanaka@users.noreply.github.com>
2023-05-12 17:46:38 -0300

    Update politica.py

--- politica.py
+++ politica.py
@@ -15,15 +15,29 @@ class Politica():
          if self.var < 0.5:
             # Escolhe um braco aleatorio entre todos os bracos
             bracosAleatorios = random.randint(0,4)
+            self.recompensas[bracosAleatorios] += 1
             print(self.bracos[bracosAleatorios])
             print(bracosAleatorios)
-         
+         elif self.var >= 0.5:
+            maior = 0 
+            for i in range(0,len(recompensas)):
+               if recompensas[i] > maior:
+                  maior = i 
+            
+            bracosAleatorios = maior 
+            print("else",self.bracos[bracosAleatorios])
+
+
+            
+            
+            
+            
          # Da uma recompensa ao braco escolhido
-         self.recompensas[bracosAleatorios] += 1
+         
          print(self.recompensas)
          print(self.var)
 
-recompensas = [1,1,1,1,1]
+recompensas = [0,0,0,0,0]
 politica = Politica(recompensas)
 #number = random(0,1)
 politica.processo()
