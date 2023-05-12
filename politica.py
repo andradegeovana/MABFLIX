9b6922679f0d922a2ba8409f54e3cc4f2a052731
SergioTanakaAtos <sergio.tanaka@atos.net>
2023-05-12 16:33:28 -0300

    Politica

--- politica.py
+++ politica.py
@@ -1,7 +1,21 @@
 import random
-class Pilitice():
-   def aaa(epsilon):
+class Politica():
+   bracos = ["acao", "aventura", "romance", "terror", "infantil"]
+   
+   def __init__(self,recompensas) -> None:
+      self.recompensas = recompensas
       
-        
-
-        
+   def processo(self):
+      self.var = random.random()
+      if self.var < 0.5:
+         bracosAleatorios = random.randint(1,5)
+         print(self.bracos[bracosAleatorios])
+         print(bracosAleatorios)
+      
+      self.recompensas[bracosAleatorios] += 1
+      print(self.recompensas)
+      print(self.var)
+recompensas = [1,1,1,1,1]
+politica = Politica(recompensas)
+#number = random(0,1)
+politica.processo()
